97e5b954b3efb0cc5093c0ce70a3320b432ed876
yadhir.maharaj <yadhir.maharaj@thesunexchange.com>
2023-03-15 11:23:51 +0200

    yadz-1 - added BreweryList component

--- client/brew-track/src/App.tsx
+++ client/brew-track/src/App.tsx
@@ -1,33 +1,79 @@
 import React, {useEffect, useState} from 'react';
+import BreweryList from './BreweryList';
 
 function App() {
 
-
-  interface BackendData {
-    users: string[];
+  interface BreweryDetails {
+    id: number,
+    breweryName :  string,
+    breweryType: string,
+    street: string,
+    city: string,
+    state: string,
+    postal_code: string,
+    country: string,
+    longitude: string,
+    latitude: string,
+    phone: string,
+    website_url: string,
+    updated_at: string,
+    tag_list: [],
+    degreesCelcius: string
   }
   
 
-  const [backEndData, setBackEndData] = useState<BackendData>()
+  const [breweryDetails, setBreweryDetails] = useState<BreweryDetails[]>([])
 
-  useEffect(()=>{
-    fetch("/api").then((response)=>{
-      response.json().then((data)=>{
-        console.log(data);
-        
-        setBackEndData(data)
-      })
-    })
-  }, [])
+  // useEffect(()=>{
+  //   fetch("/api").then((response)=>{
+  //     response.json().then((data)=>{        
+  //       setBreweryDetails(data)
+  //     })
+  //   })
+  // }, [])
+
+  useEffect(() => {
+    fetch('/api').then(response => {
+      response.json().then(data => {
+        const breweryDetails:BreweryDetails[]  = data.map((item: BreweryDetails) => {
+          return {
+            id: item.id,
+            breweryName: item.breweryName,
+            brewery_type: item.breweryType,
+            street: item.street,
+            city: item.city,
+            state: item.state,
+            postal_code: item.postal_code,
+            country: item.country,
+            longitude: item.longitude,
+            latitude: item.latitude,
+            phone: item.phone,
+            website_url: item.website_url,
+            updated_at: item.updated_at,
+            tag_list: item.tag_list,
+            degreesCelcius: item.degreesCelcius
+          };
+        });
+        setBreweryDetails(breweryDetails);
+      });
+    });
+  }, []);
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        {(typeof backEndData?.users === "undefined") ? (<p>Loading</p>) : backEndData.users.map((user, i)=>{
-          return <p key={i}>{user}</p>
-        })}
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>BrewTrack</h1>
+      <p>Find your favorite brewpubs and their air temperature</p>
+    </header>
+    <main className="App-main">
+      <h2>Breweries:</h2>
+      <BreweryList breweries={breweryDetails} />
+    </main>
+    <footer className="App-footer">
+      <p>&copy; 2023 BrewTrack</p>
+    </footer>
+  </div>
   );
 }
 
---
+++ client/brew-track/src/BreweryList.tsx
@@ -0,0 +1,86 @@
+import { Box, Grid, Paper, Typography, Button } from "@mui/material";
+import React, { ReactElement, useState } from "react";
+
+interface BreweryDetails {
+  id: number;
+  breweryName: string;
+  breweryType: string;
+  street: string;
+  city: string;
+  state: string;
+  postal_code: string;
+  country: string;
+  longitude: string;
+  latitude: string;
+  phone: string;
+  website_url: string;
+  updated_at: string;
+  tag_list: [];
+  degreesCelcius: string;
+}
+
+interface Props {
+  breweries: BreweryDetails[];
+}
+
+function BreweryList({ breweries }: Props): ReactElement {
+  const [numToShow, setNumToShow] = useState(10);
+
+  const handleShowMore = () => {
+    setNumToShow(numToShow + 10);
+  };
+
+  const visibleBreweries = breweries.slice(0, numToShow);
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        {visibleBreweries.map((brewery) => (
+          <Grid key={brewery.id} item xs={12} sm={6} md={4}>
+            <Paper sx={{ p: 2 }}>
+              <Typography variant="h6" component="h3" gutterBottom>
+                {brewery.breweryName}
+              </Typography>
+              <Typography variant="subtitle1" gutterBottom>
+                {brewery.street}
+              </Typography>
+              <Typography variant="subtitle1" gutterBottom>
+                {brewery.city}, {brewery.state} {brewery.postal_code}
+              </Typography>
+              <Box sx={{ mt: 2 }}>
+                <Typography variant="subtitle2" gutterBottom>
+                  Type: {brewery.breweryType}
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Phone: {brewery.phone}
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  {brewery.degreesCelcius} Degrees Celcius
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Website:{" "}
+                  <a
+                    href={brewery.website_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {brewery.website_url}
+                  </a>
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+      {numToShow < breweries.length && (
+        <Box sx={{ mt: 2 }}>
+          <Button variant="contained" onClick={handleShowMore}>
+            Show More
+          </Button>
+        </Box>
+      )}
+    </>
+  );
+}
+
+export default BreweryList;
